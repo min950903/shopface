4489e6f4a99df786410c3370df57c5d0aed52e1d
gmu1026 <gmu01026@gmail.com>
2020-06-08 18:13:59 +0900

    initial commit

---
+++ src/main/java/kr/ac/sunmoon/shopface/ShopfaceApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.sunmoon.shopface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopfaceApplication.class, args);
+	}
+
+}
