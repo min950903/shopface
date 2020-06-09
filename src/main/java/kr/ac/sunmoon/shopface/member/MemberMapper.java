ebeb4d27fc9e57e5874da8f702f78585dc6a27b5
gmu1026 <gmu01026@gmail.com>
2020-06-09 20:58:59 +0900

    add Mapper init version

--- src/main/java/kr/ac/sunmoon/shopface/member/MemberMapper.java
+++ src/main/java/kr/ac/sunmoon/shopface/member/MemberMapper.java
@@ -1,5 +1,14 @@
 package kr.ac.sunmoon.shopface.member;
 
-public interface MemberMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberMapper {
+	public void insert(Member member);
+	public List<Member> selectAll(Member member);
+	public Member select(Member member);
+	public void update(Member member);
+	public void delete(Member member);
 }
