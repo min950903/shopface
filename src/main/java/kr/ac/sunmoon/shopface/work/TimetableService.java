9675a68e5843f68daa03a97bd7154b50552eb514
radish1014 <taiya1014@naver.com>
2020-06-16 09:54:03 +0900

    timetable service, mapper

--- src/main/java/kr/ac/sunmoon/shopface/work/TimetableService.java
+++ src/main/java/kr/ac/sunmoon/shopface/work/TimetableService.java
@@ -5,6 +5,6 @@ import java.util.List;
 public interface TimetableService {
 	public boolean addTimetable(Timetable timetable, Schedule schedule);
 	public List<TimetableSchedule> selectTimetableList(Timetable timetable, Schedule schedule);
-	public boolean editTimetable(Timetable timetable, Schedule scheduel);
+	public boolean editTimetable(Timetable timetable, Schedule schedule);
 	public boolean removeTimetable(Schedule schedule);
 }
