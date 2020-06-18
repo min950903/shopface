c9116d6036b9a704aa9ccbcc8c33d7ceec685261
sincerefulant <sincerefulant@gmail.com>
2020-06-18 20:55:08 +0900

    last

--- src/main/java/kr/ac/sunmoon/shopface/work/TestController.java
+++ src/main/java/kr/ac/sunmoon/shopface/work/TestController.java
@@ -1,10 +1,15 @@
 package kr.ac.sunmoon.shopface.work;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,14 +28,30 @@ public class TestController {
 		return mav;
 	}
 	
-	
 	@GetMapping(value = "/test", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public List<Test> getList(Test test) {
-		List<Test> data = this.testService.getInfo();
-		return data;
-		
-	}
+	   public ResponseEntity<Map<String,Object>> getList(HttpServletRequest request) {
+	      HashMap<String, Object> map = new HashMap<String, Object>();
+	      List<Test> list = new ArrayList<>();
+	      list = this.testService.getInfo();
+	         map.put("title", list.get(0).getBranch().getName());
+	         map.put("start", list.get(0).getTimetable().getWorkStartTime());
+	         map.put("end", list.get(0).getTimetable().getWorkEndTime());
+	      return new ResponseEntity<>(map,HttpStatus.OK);
+	   }
 	
 
 }
  
+
+
+
+
+
+
+
+
+
+
+
+
+
